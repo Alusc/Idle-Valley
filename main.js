55fb3fcdbbe2127347a64497bb0c8cdb96788058
Alusc <lukasknoppamaral@gmail.com>
2025-08-03 00:39:43 -0300

    adicionadas sprites e aparecimento de pedras e ervas daninhas

--- main.js
+++ main.js
@@ -15,7 +15,17 @@ function criarLinhas() {
 function criarCampos(linha){
     for (let i = 0; i < 12; i++){
         let campo = document.createElement("div");
+        let rand = Math.random();
+        if (rand < 0.1)
+            campo.dataset.elemento = "pedra";
+        else if (rand < 0.2)
+            campo.dataset.elemento = "erva";
         campo.classList.add("campo");
+        campo.addEventListener("click", limparCampo);
         linha.appendChild(campo);
     }
+}
+
+function limparCampo(){
+    this.dataset.elemento = "";
 }
